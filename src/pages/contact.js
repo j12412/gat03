38700f461847b019f84a60ed1738f29033bfddf6
j12412 <j12412@o2.pl>
2019-08-18 14:31:29 +0200

    Change contact site

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,7 +3,7 @@ import Layout from "../components/layout"
 
 export default () => (
   <Layout>
-    <h1>I'd love to talk! Email me at the address below</h1>
+    <h1>Contact me</h1>
     <p>
       <a href="mailto:me@example.com">me@example.com</a>
     </p>
